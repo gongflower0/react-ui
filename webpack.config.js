0f47d7b0b137c6fc8e148dedfb3bee89694be7e7
mupeng <mupeng@dian.so>
2021-12-01 11:03:43 +0800

    index.tsx可以打包成index.js了

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path=require('path')
+module.exports={//node.js 模块定义
+    mode:'production',//生产模式
+    entry:{
+        index:'./lib/index.tsx'
+    },
+    output:{
+        path:path.resolve(__dirname,'dist/lib'),
+        library:'react-ui',
+        libraryTarget:'umd',//统一了amd和commonJS，libraryTarget是为了告诉webpack我要把我的代码封装成什么样的模块定义。
+    },
+    module:{
+        rules:[
+            {
+                test:/\.tsx?$/,
+                loader:'awesome-typescript-loader'//将tsx文件翻译成js文件
+            }
+        ]
+    }
+}
