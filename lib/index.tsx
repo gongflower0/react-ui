b9c0d9632e3bbab6f9a5e67cc3e91a52c0f3ecf6
mupeng <mupeng@dian.so>
2022-02-22 13:49:39 +0800

    添加onClick事件

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Icon from './icon';
 
-
+const fn=()=>{
+    console.log("fn")
+}
 ReactDOM.render(<div>
-    <Icon name="wechat"/>
-    <Icon name="alipay"/>
-    <Icon name="baidu"/>
+    <Icon name="wechat" onClick={fn}/>
 </div>,document.querySelector('#root'));
