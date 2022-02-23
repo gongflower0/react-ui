c79ff60f1fb2ad4f0f2909e399f520dce11a6fed
mupeng <mupeng@dian.so>
2022-02-23 12:13:16 +0800

    icon测试运行

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Icon from './icon';
+import Icon from './icon/icon';
 
-const fn:React.MouseEventHandler=(e)=>{
-    console.log(e)
-    console.log(e.target)
-}
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e.target);
+};
+
 ReactDOM.render(<div>
-    <Icon name="wechat" 
-    onClick={fn}
-    onMouseEnter={()=>console.log("enter")}
-    onMouseLeave={()=>console.log("leave")}
-    />
-</div>,document.querySelector('#root'));
+  <Icon name="wechat"
+        onClick={fn}
+        onMouseEnter={() => console.log('enter')}
+        onMouseLeave={() => console.log('leave')}
+        onTouchStart={() => console.log('touch')}
+  />
+</div>, document.querySelector('#root'));
